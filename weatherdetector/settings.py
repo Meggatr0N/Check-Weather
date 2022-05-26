e495e43678324b4a7fceb704794e58248b584aad
ArtB <bobkoart@gmail.com>
2022-05-26 19:29:32 +0300

    End changes to heroku

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -32,7 +32,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'django-insecure-+o#0$q&h*b#oqdmoj@td_^3yui57_@9owzb0*7=levpj(=+!%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cityweathercheck.herokuapp.com', '127.0.0.1']
 
