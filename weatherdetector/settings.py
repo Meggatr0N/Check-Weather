e4a5af2f6d70ad4d35cbc46f805ee0453024c273
ArtB <bobkoart@gmail.com>
2022-05-26 17:37:50 +0300

    make it better6

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -141,3 +141,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
